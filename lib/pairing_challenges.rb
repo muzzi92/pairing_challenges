d582899adea467f048f4335bb261c8ebd05ff623
Muzzi Aldean <amiraldean@gmail.com>
2018-04-17 09:35:04 +0100

    Completed Challenge 1: Arrays

---
+++ lib/pairing_challenges.rb
@@ -0,0 +1,12 @@
+# Challenge Set 1: Arrays
+
+# Transform [1, 2, 3, 4, 5] to [2, 3, 4, 5, 6]
+[1,2,3,4,5].drop(1).push(6)
+# Transform [1, 3, 5, 4, 2] to  [1, 2, 3, 4, 5]
+[1,3,5,4,2].sort
+# Transform [1, 3, 5, 4, 2] to [2, 3, 4, 5, 6]
+[1, 3, 5, 4, 2].drop(1).push(6).sort
+# Transform [1, 2, 3, 4, 5] to 15 (the sum)
+[1,2,3,4,5].sum
+# Transform [1, 2, 3, 4, 5] to 30 (twice the sum)
+[1,2,3,4,5].sum * 2
